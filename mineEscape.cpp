94eb12b041e8ac97d735689015a2d2e165d59218
JordanKapala <145080529+JordanKapala@users.noreply.github.com>
2024-02-20 17:42:59 -0500

    Add files via upload

--- mineEscape.cpp
+++ mineEscape.cpp
@@ -2,11 +2,13 @@
 
 #include <getopt.h>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <deque>
 #include <queue>
+#include "P2random.h"
 
 using namespace std;
 
@@ -14,6 +16,7 @@ class MineEscape {
     private:
 
         bool median_mode  = false;
+        double median;
         bool stats_mode   = false;
         int  stats_N;
         bool verbose_mode = false;
@@ -23,13 +26,17 @@ class MineEscape {
         int matrix_size;
         int srow;
         int scol;
+        int seed = -1;
+        int max_rubble = -1;
+        int TNT = -1;
+
 
         int cleared = 0;
         int total_rubble = 0;
 
         class Tile {
             private:
-                int tile_rubble;
+                int tile_rubble = 0;
                 bool discovered = false;
             
             public:
@@ -110,13 +117,13 @@ class MineEscape {
         };
 
 
-        void createMap() {
+        void createMap(istream & ss) {
             int current_rubble;
             Map.resize(matrix_size, vector<Tile>(matrix_size));
 
             for (int row = 0; row < matrix_size; row++) {
                 for (int col = 0; col < matrix_size; col++) {
-                    cin >> current_rubble;
+                    ss >> current_rubble;
                     Map[row][col] = Tile(current_rubble);
                 }
             }
@@ -124,6 +131,8 @@ class MineEscape {
 
     // priority_queue<ProxTile, vector<ProxTile>, ProxTile::PQComp> pq;
     // priority_queue<ProxTile, vector<ProxTile>, ProxTile::PQComp> TNTpq;
+    priority_queue<int, vector<int>, std::less<int>>    lower;
+    priority_queue<int, vector<int>, std::greater<int>> upper;
     vector<vector<int>> stats_vec;
         
     public:
@@ -153,6 +162,8 @@ class MineEscape {
             case 'm':
             {
                 median_mode = true;
+                upper.push(1000);
+                lower.push(0);
                 break;
             }
 
@@ -185,6 +196,20 @@ class MineEscape {
         cin >> buffer;
         cin >> srow >> scol;
 
+        stringstream ss;
+        if (input_type == 'R') {
+            cin >> buffer;
+            cin >> seed;
+            cin >> buffer;
+            cin >> max_rubble;
+            cin >> buffer;
+            cin >> TNT;
+            
+            P2random::PR_init(ss, matrix_size, seed, max_rubble, TNT);
+        }  // if ..'R'
+
+        istream &inputStream = (input_type == 'M') ? cin : ss;
+
         if (input_type != 'M' && input_type != 'R') {
             cerr << "Error: Invalid input mode" << endl;
             exit(1);
@@ -198,14 +223,14 @@ class MineEscape {
             exit(1);
         }
     
-        createMap();
+        createMap(inputStream);
     }
 
     void escape() {
         priority_queue<ProxTile, vector<ProxTile>, ProxTile::PQComp> pq;
         priority_queue<ProxTile, vector<ProxTile>, ProxTile::PQComp> TNTpq;
 
-        Tile& start = Map[srow][scol];
+        Tile start = Map[srow][scol];
         pq.push(ProxTile(start.getRubbleMap(),srow,scol));
         start.discover();
 
@@ -221,21 +246,46 @@ class MineEscape {
             col = next.getCol();
             rubble = next.getRubble();
 
-            // Tile tile = Map[row][col];
-            // int old_rubble = tile.getRubbleMap();
+            Tile& tile = Map[row][col];
 
 
             if (rubble != -1) {
-                if (rubble != 0) {
+                if (rubble != 0 && tile.getRubbleMap() == rubble) {
                     cleared++;
                     total_rubble += rubble;
-                    Map[row][col].clearRubble();
+                    tile.clearRubble();
                     if (verbose_mode) {
                         cout << "Cleared: " << rubble << " at [" << row << "," << col << "]\n";
                     }
                     if (stats_mode) {
-                        stats_vec.emplace_back(vector<int>{rubble,row,col});
+                        stats_vec.emplace_back(vector<int>{rubble,col,row});
                     }
+                    if (median_mode) {
+                            if (rubble >= upper.top()) {
+                                upper.push(rubble);
+                            }
+                            else {
+                                lower.push(rubble);
+                            }
+                            if (upper.size()-1 > lower.size()) {
+                                lower.push(upper.top());
+                                upper.pop();
+                            }
+                            else if (lower.size()-1 > upper.size()) {
+                                upper.push(lower.top());
+                                lower.pop();
+                            }
+                            if (upper.size() == lower.size()) {
+                                median = double((lower.top() + upper.top())) / 2;
+                            }
+                            else if (upper.size() > lower.size()) {
+                                median = upper.top();
+                            }
+                            else {
+                                median = lower.top();
+                            }
+                            cout << "Median difficulty of clearing rubble is: " << median << "\n";
+                        }
                 }
                 if (row == matrix_size-1 || row == 0 ||
                     col == matrix_size-1 || col == 0) {
@@ -265,7 +315,7 @@ class MineEscape {
                             pq.push(ProxTile(W.getRubbleMap(),row,col-1));
                         }
             }
-            else {
+            else if (tile.getRubbleMap() == -1) {
                 TNTpq.push(next);
                 ProxTile nextTNT = TNTpq.top();
                 // int rowt;
@@ -283,17 +333,15 @@ class MineEscape {
                     
                     tilet.clearRubble();
 
-                    // if (row == matrix_size-1 || row == 0 ||
-                    //     col == matrix_size-1 || col == 0) {
-                    //     break;
-                    // }
-
                     TNTpq.pop();
 
                     if (TNTrubble == -1) {
                         tilet.discover();
                         if (verbose_mode) {
                             cout << "TNT explosion at [" << row << "," << col << "]!\n";
+                        }
+                        if (stats_mode) {
+                        stats_vec.emplace_back(vector<int>{rubble,col,row});
                         }
 
                         if (row != 0) {
@@ -302,10 +350,12 @@ class MineEscape {
                             if (N_rubble != 0) {
                             Nt.discover();
                             TNTpq.push(ProxTile(N_rubble, row-1, col));
+                            Nt.clearRubble();
                             // pq.push(ProxTile(N_rubble, rowt-1, colt));
                             }
                             else if (!Nt.isDiscovered()) {
                                 Nt.discover();
+                                pq.push(ProxTile(0,row-1,col));
                             }
                         }
                         if (col+1 != matrix_size) {
@@ -314,34 +364,40 @@ class MineEscape {
                             if (E_rubble != 0) {
                                 Et.discover();
                                 TNTpq.push(ProxTile(E_rubble, row, col+1));
+                                Et.clearRubble();
                                 // pq.push(ProxTile(N_rubble, rowt-1, colt));
                             }
                             else if (!Et.isDiscovered()) {
                                 Et.discover();
+                                pq.push(ProxTile(0,row,col+1));
                             }
                         }
                         if (row+1 != matrix_size) {
                             Tile& St = Map[row+1][col];
                             int S_rubble = St.getRubbleMap();
                             if (S_rubble != 0) {
-                            St.discover();
-                            TNTpq.push(ProxTile(S_rubble, row+1, col));
+                                St.discover();
+                                TNTpq.push(ProxTile(S_rubble, row+1, col));
+                                St.clearRubble();
                             // pq.push(ProxTile(N_rubble, rowt-1, colt));
                             }
                             else if (!St.isDiscovered()) {
                                 St.discover();
+                                pq.push(ProxTile(0,row+1,col));
                             }
                         }
                         if (col != 0) {
                             Tile& Wt = Map[row][col-1];
                             int W_rubble = Wt.getRubbleMap();
                             if (W_rubble != 0) {
-                            Wt.discover();
-                            TNTpq.push(ProxTile(W_rubble, row, col-1));
+                                Wt.discover();
+                                TNTpq.push(ProxTile(W_rubble, row, col-1));
+                                Wt.clearRubble();
                             // pq.push(ProxTile(N_rubble, rowt-1, colt));
                             }
                             else if (!Wt.isDiscovered()) {
                                 Wt.discover();
+                                pq.push(ProxTile(0,row,col-1));
                             }
                         }
                     }
@@ -352,8 +408,40 @@ class MineEscape {
                         if (verbose_mode) {
                             cout << "Cleared by TNT: " << TNTrubble << " at [" << row << "," << col << "]\n";
                         }
+                        if (stats_mode) {
+                            stats_vec.emplace_back(vector<int>{TNTrubble,col,row});
+                        }
+                        if (median_mode) {
+                            if (TNTrubble >= upper.top()) {
+                                upper.push(TNTrubble);
+                            }
+                            else {
+                                lower.push(TNTrubble);
+                            }
+                            if (upper.size()-1 > lower.size()) {
+                                lower.push(upper.top());
+                                upper.pop();
+                            }
+                            else if (lower.size()-1 > upper.size()) {
+                                upper.push(lower.top());
+                                lower.pop();
+                            }
+                            if (upper.size() == lower.size()) {
+                                median = double((lower.top() + upper.top())) / 2;
+                            }
+                            else if (upper.size() > lower.size()) {
+                                median = upper.top();
+                            }
+                            else {
+                                median = lower.top();
+                            }
+                            cout << "Median difficulty of clearing rubble is: " << median << "\n";
+                        }
                     }
                 }
+            }
+            else {
+                pq.pop();
             }
         }
     }
@@ -364,23 +452,24 @@ class MineEscape {
             if (int(stats_vec.size()) < stats_N) {
                 stats_N = int(stats_vec.size());
             }
+            int dist = int(stats_vec.size()-stats_N);
             cout << "First tiles cleared:\n";
             for (int i = 0; i < stats_N; i++) {
                 if (stats_vec[i][0] == -1) {
-                    cout << "TNT at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << "TNT at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
                 else {
-                    cout << stats_vec[i][0] << " at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << stats_vec[i][0] << " at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
             }
 
             cout << "Last tiles cleared:\n";
-            for (int i = stats_N-1; i >= 0; i--) {
+            for (int i = int(stats_vec.size()-1); i >= dist; i--) {
                 if (stats_vec[i][0] == -1) {
-                    cout << "TNT at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << "TNT at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
                 else {
-                    cout << stats_vec[i][0] << " at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << stats_vec[i][0] << " at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
             }
             
@@ -388,20 +477,20 @@ class MineEscape {
             sort(stats_vec.begin(), stats_vec.end());
             for (int i = 0; i < stats_N; i++) {
                 if (stats_vec[i][0] == -1) {
-                    cout << "TNT at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << "TNT at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
                 else {
-                    cout << stats_vec[i][0] << " at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << stats_vec[i][0] << " at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
             }
 
             cout << "Hardest tiles cleared:\n";
-            for (int i = stats_N-1; i >= 0; i--) {
+            for (int i = int(stats_vec.size()-1); i >= dist; i--) {
                 if (stats_vec[i][0] == -1) {
-                    cout << "TNT at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << "TNT at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
                 else {
-                    cout << stats_vec[i][0] << " at [" << stats_vec[i][1] << "," << stats_vec[i][2] << "]\n";
+                    cout << stats_vec[i][0] << " at [" << stats_vec[i][2] << "," << stats_vec[i][1] << "]\n";
                 }
             }
         }
@@ -411,7 +500,7 @@ class MineEscape {
 int main(int argc, char *argv[])
 {
     std::ios_base::sync_with_stdio(false);
-    //cout << std::fixed << std::setprecision(2);
+    cout << std::fixed << std::setprecision(2);
     MineEscape object = MineEscape();
     object.GetOptions(argc, argv);
     object.readMine();
